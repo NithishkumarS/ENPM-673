86127c16d3085d993abc254308cdaeab0be870ac
Rama Prashanth <rama@wireless-10-104-183-32.umd.edu>
2019-02-27 22:55:00 -0500

    clean

--- Homography_AR/homography.py
+++ Homography_AR/homography.py
@@ -6,11 +6,11 @@ import numpy as np
 def homographicTransform(cornerPoints, Xc):
     contourCount = len(cornerPoints) / 4
     if contourCount == 1:
-        Xw = cornerPoints #np.concatenate((cornerPoints[corner[0]], cornerPoints[corner[1]],
-             # cornerPoints[corner[2]], cornerPoints[corner[3]]), axis=0)
-        print('Xw')
-        print(Xw)
-        #print(np.asmatrix(Xw))
+        Xw = cornerPoints  # np.concatenate((cornerPoints[corner[0]], cornerPoints[corner[1]],
+        # cornerPoints[corner[2]], cornerPoints[corner[3]]), axis=0)
+        # print('Xw')
+        # print(Xw)
+        # print(np.asmatrix(Xw))
 
     A = np.zeros((1, 9), int)
     temp = np.zeros((1, 9), int)
@@ -30,16 +30,16 @@ def homographicTransform(cornerPoints, Xc):
         else:
             A = np.append(A, temp, axis=0)
 
-    #print(A)
+    # print(A)
     U, s, V = np.linalg.svd(A)
 
-    h = V[-1,:]/V[-1,-1]
-    #print(h)
+    h = V[-1, :]/V[-1, -1]
+    # print(h)
     H = (np.reshape(h, (3, 3)))
     # H = H/H[2][2]
     #print('H from cal')
-    #print(H)
-    #print(np.linalg.inv(H))
+    # print(H)
+    # print(np.linalg.inv(H))
     # H_cv, status = cv2.findHomography(np.asmatrix(Xw), np.asmatrix(Xc))
    # print('homography from function')
    # print(H_cv)
--- Homography_AR/part_2.2.py
+++ Homography_AR/part_2.2.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
-__author__ = "Nantha Kumar Sunder, Nithish Kumar"
+__author__ = "Nantha Kumar Sunder, Nithish Kumar, Rama Prashanth"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import numpy as np
+import os
+import sys
+
+import cv2
 import matplotlib.pyplot as plt
-import os, sys
+import numpy as np
+
+from ARTag_Decoder import decode
+from getCornerPoints import getCornerPoints
+from homography import homographicTransform
+from part_1 import getTransfomredImage, getVideoFile
+from virtualCube import virtualCube
 
 # This try-catch is a workaround for Python3 when used with ROS; it is not needed for most platforms
 try:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 except:
     pass
-import cv2
-from homography import homographicTransform
-from ARTag_Decoder import decode
-from getCornerPoints import getCornerPoints
-from part_1 import getTransfomredImage
-from part_1 import getVideoFile
-from virtualCube import virtualCube
+
 
 def main():
     """ Main entry point of the app """
-    usr_input = input('Select the Video\n\n\t1. Tag0 \n\t2. Tag1 \n\t3. Tag2 \n\t4. multipleTags')
+    usr_input = input(
+        'Select the Video\n\t1. Tag0 \n\t2. Tag1 \n\t3. Tag2 \n\t4. multipleTags\n\nYour Choice: ')
     print(getVideoFile(int(usr_input)))
     cap = cv2.VideoCapture(getVideoFile(int(usr_input)))
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -32,16 +36,17 @@ def main():
         ret, frame = cap.read()
         cv2.imshow('Normal', frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray,(5,5), 0)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
         corner_points, dst_total, frame = getCornerPoints(frame)
         for tag_no in range(0, np.int(len(corner_points)/4)):
-            H = homographicTransform(corner_points[4*tag_no:4*tag_no+4][:],Xc)
-            virtualCube(H,frame,corner_points[4*tag_no:4*tag_no+4][:])
+            H = homographicTransform(corner_points[4*tag_no:4*tag_no+4][:], Xc)
+            virtualCube(H, frame, corner_points[4*tag_no:4*tag_no+4][:])
         #cv2.imshow('Superimposed', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
