0b3db4a8be8d15180546a20c23d185a8bee29996
nantha007 <nantha07@terpmail.umd.edu>
2019-03-07 15:56:23 -0500

    Plotting histogram

--- Lane_Detection/part1.py
+++ Lane_Detection/part1.py
@@ -35,17 +35,15 @@ def main():
     cap = cv2.VideoCapture(getVideoFile(int(usr_input)))
     font = cv2.FONT_HERSHEY_SIMPLEX
     Xc = np.array([[149, 0], [249, 0], [249, 399], [149, 399]])
-    Xw = np.array([[548, 518], [761, 522], [891, 616], [408, 616]])
+    #Xw = np.array([[548, 518], [761, 522], [891, 616], [408, 616]])
+    Xw = np.array([[600, 452], [683, 452], [1005, 630], [387, 630]])
     while(cap.isOpened()):
         ret, frame = cap.read()
         if frame is not None:
             frame = np.array(frame, dtype=np.uint8)
-            segmented_image = colorSegmentation(frame)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (5, 5), 0)
             image_shape = gray.shape
-            cropped_image = segmented_image.copy()
-            cropped_image[0:int(image_shape[0]*2/3),:] = 1
 
             '''
             ret, thresh = cv2.threshold(cropped_image, 150, 255, 0, cv2.THRESH_BINARY)
@@ -54,15 +52,21 @@ def main():
             except:
                 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             '''
-            '''
-            Homography = homographicTransform(Xw, Xc)
-            transformed_image = getTransfomredImage(np.linalg.inv(Homography[0]), gray, 400)
-            undistorted_img = get_undistort(transformed_image)
-            '''
 
+            undistorted_img = get_undistort(frame)
+            segmented_image = colorSegmentation(undistorted_img)
+
+            cropped_image = segmented_image.copy()
+            cropped_image[0:int(image_shape[0]*1/2),:] = 1
+
+            Homography = homographicTransform(Xw, Xc)
+            transformed_image = getTransfomredImage(np.linalg.inv(Homography[0]), segmented_image, 400,400)
+            hist = cv2.calcHist([transformed_image],[0],None,[256],[0,256])
             edges = cv2.Canny(cropped_image,100,200)
             #cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
-            cv2.imshow('transformed_image', edges)
+            cv2.imshow('transformed_image', transformed_image)
+            plt.plot(hist)
+            plt.show()
             cv2.imshow('Lane Detection', frame)
             if cv2.waitKey(0) & 0xFF == ord('q'):
                 break
