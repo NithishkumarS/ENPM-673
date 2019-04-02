7ce60f3c44139226042a00e63b3c98ea3180e693
Rama Prashanth <rv@ramaprashanth.com>
2019-04-02 12:36:01 -0400

    1D Gaussian segmentation

---
+++ Buoy_Detection/gaussian.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt, pi, exp
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+np.set_printoptions(threshold=sys.maxsize)
+
+class Gaussian:
+    """
+    Model color distribution using 1D Gaussian
+    """
+    mean_g = 0
+    mean_r = 0
+    mean_y = 0
+    variance_g = 0
+    variance_r = 0
+    variance_y = 0
+
+    def __init__(self):
+        green_buoy_images = []
+        red_buoy_images = []
+        yellow_buoy_images = []
+
+        # Load Data Set
+        for file in os.listdir("DataSet/Green"):
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                green_buoy_images.append("DataSet/Green/" + file)
+        for file in os.listdir("DataSet/Red"):
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                red_buoy_images.append("DataSet/Red/" + file)
+        for file in os.listdir("DataSet/Yellow"):
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                yellow_buoy_images.append("DataSet/Yellow/" + file)
+
+        self.mean_g, self.variance_g = self.get_mean_gaussians(green_buoy_images)
+        self.mean_r, self.variance_r = self.get_mean_gaussians(red_buoy_images)
+        self.mean_y, self.variance_y = self.get_mean_gaussians(yellow_buoy_images)
+
+        # print(self.mean_g, self.mean_r, self.mean_y)
+        # print(self.variance_g, self.variance_r, self.variance_y)
+
+    def get_histogram(self, img, channel):
+        hist = cv2.calcHist([img[:,:,channel]], [0], None, [256], [0,256])
+        return hist
+
+    def get_mean(self, hist):
+        sum = 0
+        n = 0
+        for i in range(hist.shape[0]):
+            sum += (i * hist[i][0])
+            n += hist[i][0]
+        mean = sum/n
+        return mean
+
+    def get_variance(self, hist, mean):
+        num = 0
+        n = 0
+        for i in range(hist.shape[0]):
+            num += (hist[i][0])*((i - mean)**2)
+            n += hist[i][0]
+        variance = num/n
+        return variance
+
+    def get_gaussian(self, hist):
+        mean = self.get_mean(hist)
+        variance = self.get_variance(hist, mean)
+        return mean, variance
+
+    def get_mean_gaussians(self, images):
+        hists_b = np.zeros((256,1))
+        hists_g = np.zeros((256,1))
+        hists_r = np.zeros((256,1))
+
+        for image in images:
+            img = cv2.imread(image)
+            hists_b = np.add(hists_b, self.get_histogram(img, 0))
+            hists_g = np.add(hists_g, self.get_histogram(img, 1))
+            hists_r = np.add(hists_r, self.get_histogram(img, 2))
+
+        mean_b, variance_b = self.get_gaussian(hists_b)
+        mean_g, variance_g = self.get_gaussian(hists_g)
+        mean_r, variance_r = self.get_gaussian(hists_r)
+        mean = [mean_b, mean_g, mean_r]
+        variance = [variance_b, variance_g, variance_r]
+        return mean, variance
+
+    def get_pdf(self, x, mean, variance):
+        """
+        Probability Density Function
+        # https://stackoverflow.com/questions/809362/how-to-calculate-cumulative-normal-distribution-in-python/809406
+        """
+        # var = float(variance)
+        # denom = (2*pi*var)**0.5
+        # num = exp(-(float(x)-float(mean))**2/(2*var))
+        # return num/denom
+
+        # TOO SLOW ---> Need to replace
+        # https://stackoverflow.com/questions/809362/how-to-calculate-cumulative-normal-distribution-in-python
+        cdf = norm.cdf(x, mean, sqrt(variance))
+        return cdf
+
+    def get_thresholded_pdf(self, frame, p1, p2, p3):
+        # q = np.multiply(np.multiply(np.multiply(frame, p1>thresh1), p2>thresh2), p3>thresh3)
+        # q1 = (p1>0.00804).astype(int)
+        # q2 = (p1<0.0104).astype(int)
+
+        # q3 = (p2>0.011).astype(int)
+        # q4 = (p2<0.012).astype(int)
+        #
+        # q5 = (p3>0.008).astype(int)
+        # q6 = (p3<0.0091).astype(int)
+        # q = np.multiply(q1, np.multiply(q2, np.multiply(np.multiply(q3, np.multiply(q4, q5)), q6))).astype(np.uint8)
+        q = np.multiply(frame, p1 > 0.98)
+        return q
+
+    def detect_buoys(self, frame):
+        frame_b = frame[:,:,0]
+        frame_g = frame[:,:,1]
+        frame_r = frame[:,:,2]
+
+        # Function mapping
+        # https://stackoverflow.com/questions/35215161/most-efficient-way-to-map-function-over-numpy-array
+
+        # For Green Buoy - RBG PDF
+        map_fn_g_b = lambda x:self.get_pdf(x, self.mean_g[0], self.variance_g[0])
+        fn_g_b = np.vectorize(map_fn_g_b)
+        pdf_g_b = fn_g_b(frame_b)
+
+        map_fn_g_g = lambda x:self.get_pdf(x, self.mean_g[1], self.variance_g[1])
+        fn_g_g = np.vectorize(map_fn_g_g)
+        pdf_g_g = fn_g_g(frame_g)
+
+        map_fn_g_r = lambda x:self.get_pdf(x, self.mean_g[2], self.variance_g[2])
+        fn_g_r = np.vectorize(map_fn_g_r)
+        pdf_g_r = fn_g_r(frame_r)
+
+        # For Red Buoy - RBG PDF
+        map_fn_r_b = lambda x:self.get_pdf(x, self.mean_r[0], self.variance_r[0])
+        fn_r_b = np.vectorize(map_fn_r_b)
+        pdf_r_b = fn_r_b(frame_b)
+
+        map_fn_r_g = lambda x:self.get_pdf(x, self.mean_r[1], self.variance_r[1])
+        fn_r_g = np.vectorize(map_fn_r_g)
+        pdf_r_g = fn_r_g(frame_g)
+
+        map_fn_r_r = lambda x:self.get_pdf(x, self.mean_r[2], self.variance_r[2])
+        fn_r_r = np.vectorize(map_fn_g_r)
+        pdf_r_r = fn_r_r(frame_r)
+
+        # plt.figure(1)
+        # x = np.linspace(0,256,256)
+        # plt.plot(x,norm.pdf(x, self.mean_r[2], self.variance_r[2]),"r-")
+        # plt.hist(frame_r.ravel(),256,(0,256))
+        # plt.show()
+
+        # For Yellow Buoy - RBG PDF
+        map_fn_y_b = lambda x:self.get_pdf(x, self.mean_y[0], self.variance_y[0])
+        fn_y_b = np.vectorize(map_fn_y_b)
+        pdf_y_b = fn_y_b(frame_b)
+
+        map_fn_y_g = lambda x:self.get_pdf(x, self.mean_y[1], self.variance_y[1])
+        fn_y_g = np.vectorize(map_fn_y_g)
+        pdf_y_g = fn_y_g(frame_g)
+
+        map_fn_y_r = lambda x:self.get_pdf(x, self.mean_y[2], self.variance_y[2])
+        fn_y_r = np.vectorize(map_fn_y_r)
+        pdf_y_r = fn_y_r(frame_r)
+
+        # Thresold by Tial and Error
+
+        print('R max ',np.amax(pdf_r_r))
+        print('G max ',np.amax(pdf_r_g))
+        print('B max ',np.amax(pdf_r_b))
+
+        # Max PDF G G 0.00063265
+        _frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        print('Noise')
+        print(pdf_r_r[302,205])
+        print(pdf_r_g[302,205])
+        print(pdf_r_b[302,205])
+
+        print('Buoy')
+        print(pdf_r_r[199,338])
+        print(pdf_r_g[199,338])
+        print(pdf_r_b[199,338])
+        _frame_g = self.get_thresholded_pdf(frame_r, pdf_r_r, pdf_r_g, pdf_r_b)
+
+        # R 0.0105136
+        # G 0.01210605
+        # B 0.009138
+
+        # Max PDF G B 0.00048706450619803384
+        # _frame_b = self.get_thresholded_pdf(frame_b, pdf_g_b, 0.00048)
+        # Max PDF G R 0.0002954202761348719
+        # _frame_r = self.get_thresholded_pdf(frame_r, pdf_g_r, 0.00029)
+        # print(p)
+        # print(np.array([_frame_b, _frame_g, _frame_r]))
+
+        return _frame_g
