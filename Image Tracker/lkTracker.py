ed8783f22cfeffe6d1915008379b1d9c9ef310b4
Nithish <nithishtimes@gmail.com>
2019-04-12 15:33:09 -0400

    SD parameters estimation

--- Image Tracker/lkTracker.py
+++ Image Tracker/lkTracker.py
@@ -21,35 +21,70 @@ def computeJacobian(x,y):
 def affineLKtracker(frame, tmpImg, cornerPoints, prevWarp):
     frame = cv2.warpAffine(frame, prevWarp, (frame.shape[1], frame.shape[0]))
 #     cv2.imshow('frame after',frame)  
+    input_frame = frame[cornerPoints[0][1]:cornerPoints[2][1],cornerPoints[0][0]:cornerPoints[2][0] ]
     template = tmpImg[cornerPoints[0][1]:cornerPoints[2][1],cornerPoints[0][0]:cornerPoints[2][0] ]
     
+    diff = template - input_frame
+
     #gradient X and Y
-    gradX = cv2.Sobel(frame, cv2.CV_32F, 1, 0, ksize=5)
-    gradY = cv2.Sobel(frame, cv2.CV_32F, 0, 1, ksize=5)
+    gradX = cv2.Sobel(input_frame, cv2.CV_32F, 1, 0, ksize=5)
+    gradY = cv2.Sobel(input_frame, cv2.CV_32F, 0, 1, ksize=5)
     cv2.imshow('gradX',gradX)
     cv2.imshow('gradY',gradY)
     
-    print(frame.shape)
-    op1 = np.zeros_like(frame)
-    op = np.zeros((frame.shape[0],frame.shape[1]*6))
-    print(op.shape)
+    print(input_frame.shape)
+    op1 = np.zeros_like(input_frame)
+    op = np.zeros(input_frame.shape[0]*input_frame.shape[1]*6)
+#     op = np.zeros((input_frame.shape[0],input_frame.shape[1]))*6
+    op = op.reshape((6,input_frame.shape[0], input_frame.shape[1]))
+    print('op size:',op.shape)
     
     # Steepest Descent 
-    for x in range(0,frame.shape[1]):
-        for y in range(0,frame.shape[0]):
+    for x in range(0,input_frame.shape[1]):
+        for y in range(0,input_frame.shape[0]):
             tmp =  np.matmul(np.array([ [gradX[y][x],gradY[y][x]] ]),computeJacobian(x, y))#.astype(np.float32)
             
             for i in range(6):
-                op[y][x+(i*720)-1]=tmp[0][i]
-#     cv2.imshow('op',op)
+                op[i][y][x]=tmp[0][i]
+
+#                 op[y][x+(i*input_frame.shape[1])-1]=tmp[0][i]
+    output_jacob = op.reshape((input_frame.shape[0]*6, input_frame.shape[1]))
+    cv2.imshow('op',output_jacob)
     
     # Compute Hessian
+    Hessian = np.zeros((input_frame.shape[1],input_frame.shape[1])).astype(np.float64)
+    SD_param = np.zeros((input_frame.shape[1],input_frame.shape[1])).astype(np.float64)
+    print('Hessian',Hessian.shape)
     
-    Hessian = np.matmul(op,np.transpose(op))          # (nx6m) x (6mxn)  =nxn 
-#     cv2.imshow('Hessian',Hessian)
+    for i in range(6):
+        inp = np.matmul(op[i].T,op[i])
+        Hessian += inp
+#     Hessian = [[np.sum(np.multiply(op[a].T, op[b])) for a in range(6)] for b in range(6)]
+    print(Hessian.shape)
+#     np.matmul(op,np.transpose(op))          # (nx6m) x (6mxn)  =nxn 
+    cv2.imshow('Hessian',Hessian)
+    Hessian_Inv = np.linalg.inv(Hessian)
     
-    HessianInv = np.linalg.inv(Hessian)
+    cv2.imshow('HessianInv',Hessian_Inv)
     
+    SD_param = np.zeros(6)
+    for i in range(6):
+        inp = np.matmul(op[i].T,diff)
+        SD_param[i] = np.sum(inp)
+    print(SD_param)
+    
+    delp = np.matmul(Hessian_Inv,SD_param)
+    print(delp.shape)
+#   
+    
+    
+    '''
+    for i in range(6):
+        op[y][x+(i*input_frame.shape[1])-1]
+
+    '''
+    
+ 
     
     error = 1
     Warp = prevWarp
