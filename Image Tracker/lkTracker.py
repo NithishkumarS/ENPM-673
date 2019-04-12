f4157c7893beddee99e63c5285a6c329958a735c
Nithish <nithishtimes@gmail.com>
2019-04-12 01:11:45 -0400

    Hessian computation added

--- Image Tracker/lkTracker.py
+++ Image Tracker/lkTracker.py
@@ -30,19 +30,27 @@ def affineLKtracker(frame, tmpImg, cornerPoints, prevWarp):
     cv2.imshow('gradY',gradY)
     
     print(frame.shape)
-    op1 = np.zeros_like(frame,dtype = np.float32)
-    op = np.zeros((frame.shape[0],frame.shape[1]*6),dtype = np.float32)
+    op1 = np.zeros_like(frame)
+    op = np.zeros((frame.shape[0],frame.shape[1]*6))
     print(op.shape)
     
     # Steepest Descent 
     for x in range(0,frame.shape[1]):
         for y in range(0,frame.shape[0]):
-            tmp =  np.matmul(np.array([[x,y]]),computeJacobian(x, y)).astype(np.float32)
+            tmp =  np.matmul(np.array([ [gradX[y][x],gradY[y][x]] ]),computeJacobian(x, y))#.astype(np.float32)
             
             for i in range(6):
                 op[y][x+(i*720)-1]=tmp[0][i]
-    cv2.imshow('op',op)
-    print(op1)
+#     cv2.imshow('op',op)
+    
+    # Compute Hessian
+    
+    Hessian = np.matmul(op,np.transpose(op))          # (nx6m) x (6mxn)  =nxn 
+#     cv2.imshow('Hessian',Hessian)
+    
+    HessianInv = np.linalg.inv(Hessian)
+    
+    
     error = 1
     Warp = prevWarp
     return cornerPoints, Warp, error
