5467af81a5aa94ca943ea1bfc5d99bd88b37e4ce
Nithish <nithishtimes@gmail.com>
2019-05-04 14:13:25 -0400

    Fundamental matrix computation update

--- Visual_Odometry/fundamentalMatrix.py
+++ Visual_Odometry/fundamentalMatrix.py
@@ -6,10 +6,16 @@ def computeFundamentalMatrix(P1, P2):
     for i in range(8):
         A.append([P1[i][0]*P2[i][0] , P1[i][0]*P2[i][1], P1[i][0], P1[i][1]*P2[i][0], P1[i][1]*P2[i][1], P2[i][1], P2[i][0] , P2[i][1], 1 ])
     A = np.array(A)
-    U, s, V = np.linalg.svd(A)
-    h = V[-1,:]/V[-1,-1]
-    H = (np.reshape(h, (3, 3)))
-    return H
+    U, s, Vt = np.linalg.svd(A)
+    h = Vt[-1,:]
+    F = (np.reshape(h, (3, 3)))
+    magF = np.linalg.norm(F, np.inf)
+    F = F / magF
+    FU , Fs, FV = np.linalg.svd(F)
+    Fs[2][2] = 0
+    F_hat = np.matmul(np.matmul(FU,Fs),FV)
+    return F_hat
+
 
 def ransac(P1,P2):
     n_iters = 1000
