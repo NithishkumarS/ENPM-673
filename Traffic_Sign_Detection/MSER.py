70a1468145a470d52f817e30be07ce0a0892f0e6
nantha007 <nantha07@terpmail.umd.edu>
2019-05-17 20:10:34 -0400

    MSER updated

--- Traffic_Sign_Detection/MSER.py
+++ Traffic_Sign_Detection/MSER.py
@@ -1,14 +1,82 @@
 import numpy as np
+try:
+    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+except:
+    pass
 import cv2
+from boundingBox import *
 
+def getInp(new_img):
+    mask_blue, mask_red = colorSegmentation(new_img)
+    # kernel = np.ones((3,3),np.uint8)
+    # erosion = cv2.erode(mask_blue,kernel,iterations = 1)
+    # erosion = cv2.Canny(erosion,100,200)
+    # blackhat = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
+    imb = cv2.cvtColor(mask_blue, cv2.COLOR_BGR2GRAY)
 
+    imr = cv2.cvtColor(mask_red, cv2.COLOR_BGR2GRAY)
+    im = imb | imr
+    return imb, imr
 
-def MSER(inp,new_img):
-    cur_image = new_img.copy()
-    mser = cv2.MSER_create(_delta = 5, _min_diversity = .7, _max_variation = .2)
-    regions, boxes = mser.detectRegions(inp)
+# def hisEqulColor(img):
+#     ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+#     channels = cv2.split(ycrcb)
+#     cv2.equalizeHist(channels[0], channels[0])
+#     cv2.merge(channels, ycrcb)
+#     cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR, img)
+#     return img
+
+def masker(img):
+    img[int(img.shape[0]/2):,:] = 0
+    return img
+
+def contrastNormalize(new_img):
+    r_channel = new_img[:,:,2]
+    g_channel = new_img[:,:,1]
+    b_channel = new_img[:,:,0]
+
+    arr = np.asarray(r_channel)
+    r_channel = imadjust(arr,arr.min(),arr.max(),0,1).astype(np.float32)
+    arr = np.asarray(g_channel)
+    g_channel = imadjust(arr,arr.min(),arr.max(),0,1).astype(np.float32)
+    arr = np.asarray(b_channel)
+    b_channel = imadjust(arr,arr.min(),arr.max(),0,1).astype(np.float32)
+
+    sum = r_channel + g_channel + b_channel
+    sum[sum==0] = 1
+
+    r_channel = np.divide(r_channel, sum)
+    g_channel = np.divide(g_channel, sum)
+    b_channel = np.divide(b_channel, sum)
+
+    imr = np.maximum(0,np.minimum((r_channel-b_channel),(r_channel-g_channel)))
+    imb = np.maximum(0,b_channel-r_channel)#,(b_channel-g_channel)))
+
+    return imr, imb
+
+def imadjust(x,a,b,c,d,gamma=1):
+    y = (((x - a) / (b - a)) ** gamma) * (d - c) + c
+    return y
+
+def boundingBox_mser(new_img):
+    imr, imb = contrastNormalize(new_img)
+    # new_img = MSER(imr, new_img)
+    new_img = MSER(imb, new_img)
+    return new_img
+
+def MSER(img, new_img):
+    img = masker(img)
+    img = np.uint8(img)
+    new_img = new_img.copy()
+    mser = cv2.MSER_create(_delta = 2, _min_diversity = 0.8, _max_variation = .2)
+    # img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    regions, boxes = mser.detectRegions(img)
     for p in regions:
         xmax, ymax = np.amax(p, axis=0)
         xmin, ymin = np.amin(p, axis=0)
-        cv2.rectangle(cur_image, (xmin,ymax), (xmax,ymin), (0, 255, 0), 1)
-    return cur_image
+        # w = xmax - xmin
+        # h = ymax - ymin
+        # if h >= 0.9*w and w*h > 100 and (h < 2.5*w) and w*h < 30000:
+        #     cv2.rectangle(new_img, (xmin,ymax), (xmax,ymin), (0, 255, 0), 1)
+        cv2.rectangle(new_img, (xmin,ymax), (xmax,ymin), (0, 255, 0), 1)
+    return new_img
--- Traffic_Sign_Detection/MSER_main.py
+++ Traffic_Sign_Detection/MSER_main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from openpyxl.drawing.image import bounding_box
 __author__ = "Nantha Kumar Sunder, Nithish Kumar"
 __version__ = "0.1.0"
 __license__ = "MIT"
@@ -30,66 +29,19 @@ def loadImages():
         imageList[i] = "TSR/ProcessedInput/" + str(imageList[i]) + ".jpg"
     return imageList
 
-def denoise(new_img):
-    dst = cv2.fastNlMeansDenoisingColored(new_img,None,10,10,7,21)
-    return dst
-
-def getInp(new_img):
-    mask_blue, mask_red = colorSegmentation(new_img)
-    # kernel = np.ones((3,3),np.uint8)
-    # erosion = cv2.erode(mask_blue,kernel,iterations = 1)
-    # erosion = cv2.Canny(erosion,100,200)
-    # blackhat = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
-    imb = cv2.cvtColor(mask_blue, cv2.COLOR_BGR2GRAY)
-
-    imr = cv2.cvtColor(mask_red, cv2.COLOR_BGR2GRAY)
-    im = imb | imr
-    return im 
 
 def main():
     """ Main entry point of the app """
-    frameCount = 880
+    frameCount = 85
     imageList = loadImages()
     while frameCount < len(imageList):
         new_img = cv2.imread(imageList[frameCount])
-        
-        r_channel = new_img[:,:,2]
-        g_channel = new_img[:,:,1]
-        b_channel = new_img[:,:,0]
-        
-        sum = r_channel + g_channel + b_channel
-        sum[sum==0] = 1
-        r_channel = np.divide(r_channel, sum)
-        g_channel = np.divide(b_channel, sum)
-        b_channel = np.divide(b_channel, sum)
-        
-        r = np.uint8(np.maximum(0,np.minimum((r_channel-b_channel),(r_channel-g_channel)) ) )
-        b = np.uint8(np.maximum(0,np.minimum((b_channel-r_channel),(b_channel-g_channel)) ) )
-        
-        ret, rr = cv2.threshold(r, 240, 255, cv2.THRESH_BINARY)
-        ret, bb = cv2.threshold(b, 240, 255, cv2.THRESH_BINARY)
-        rb = r | b
-
-        cv2.imshow('rb',r)
-        
-        cv2.imshow('frame', new_img)
-#         cv2.imshow('r',r)
-#         cv2.waitKey(0)
-         
-#         r = np.copy(new_img)        #  r - 35-45    g- 60-70   b-100 - 115     
-#         r[:,:,1] = 0
-#         r[:,:,2] = 0
-#         ret,th1 = cv2.threshold(r[:,:,0],100,115,cv2.THRESH_BINARY)
-#         cv2.imshow('red_channel',th1)
-#         inp = boundingBox(new_img)
-        
-#         inp = getInp(new_img)    
-        frame = MSER(b, new_img)
-#         boundingBox(new_img)
+        frame = boundingBox_mser(new_img)
+        frame = cv2.resize(frame,(800,600))
+        new_img = cv2.resize(new_img,(800,600))
         cv2.imshow('frame', frame)
-#         cv2.imshow('mask', inp)
-        # cv2.imshow('segmented', res)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.imshow('Input Image',new_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         frameCount = frameCount + 1
         print(frameCount)
@@ -97,3 +49,35 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    '''
+    r_channel = new_img[:,:,2]
+    g_channel = new_img[:,:,1]
+    b_channel = new_img[:,:,0]
+
+    sum = r_channel + g_channel + b_channel
+    sum[sum==0] = 1
+    r_channel = np.divide(r_channel, sum)
+    g_channel = np.divide(b_channel, sum)
+    b_channel = np.divide(b_channel, sum)
+
+    r = np.uint8(np.maximum(0,np.minimum((r_channel-b_channel),(r_channel-g_channel)) ) )
+    b = np.uint8(np.maximum(0,np.minimum((b_channel-r_channel),(b_channel-g_channel)) ) )
+
+    ret, rr = cv2.threshold(r, 240, 255, cv2.THRESH_BINARY)
+    ret, bb = cv2.threshold(b, 240, 255, cv2.THRESH_BINARY)
+    rb = r | b
+
+    cv2.imshow('rb',r)
+
+    cv2.imshow('frame', new_img)
+    cv2.imshow('r',r)
+    cv2.waitKey(0)
+
+    r = np.copy(new_img)        #  r - 35-45    g- 60-70   b-100 - 115
+    r[:,:,1] = 0
+    r[:,:,2] = 0
+    ret,th1 = cv2.threshold(r[:,:,0],100,115,cv2.THRESH_BINARY)
+    cv2.imshow('red_channel',th1)
+    inp = boundingBox(new_img)
+    '''
