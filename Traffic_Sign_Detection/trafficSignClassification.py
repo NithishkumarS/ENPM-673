220c25d8679f95555c5f3738713ca40efe171d9e
Nithish <nithishtimes@gmail.com>
2019-05-17 20:34:42 -0400

    One class with 95% accuracy in rejecting extra features

--- Traffic_Sign_Detection/trafficSignClassification.py
+++ Traffic_Sign_Detection/trafficSignClassification.py
@@ -6,6 +6,7 @@ __license__ = "MIT"
 import os
 import sys
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import csv
 # This try-catch is a workaround for Python3 when used with ROS; it is not needed for most platforms
@@ -26,7 +27,8 @@ def getFolderList(fol_str):
         if not basename:
             folderList.append(filename)
     folderList = sorted(folderList, key = last_4chars)
-    for i in range(len(folderList)):
+    for i in range(len(folderList)):   
+        print('i:',i)  
         folderList[i] ="TSR/" + fol_str + "/" + folderList[i] + "/"
 
     return folderList
@@ -72,6 +74,65 @@ def getHOG():
     hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels, signedGradients)
     return hog
 
+def train_traffic_signs():
+    hog = getHOG()
+    dataset = []
+    datalabels = []
+    folderCount = 0
+    folderList = getFolderList("Training")
+    dataCount = 0
+    while folderCount < 5:#len(folderList):
+        imageCount = 0
+        imageList, prop = loadImages(folderList[folderCount])
+        classId = prop[0][-1]
+        while imageCount < len(imageList):
+            new_img = cv2.imread(folderList[folderCount] + prop[imageCount][0])
+            new_img = resize(new_img, prop[imageCount])
+            des = hog.compute(new_img)
+            dataset.append(des)
+            datalabels.append(int(1))
+            # print(int(classId))
+            cv2.imshow('frame', new_img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            imageCount = imageCount + 1
+            dataCount = dataCount + 1
+            # print(imageCount)
+        folderCount = folderCount + 1
+        cv2.destroyAllWindows()
+    
+    # Set up SVM for OpenCV 3
+    svm = cv2.ml.SVM_create()
+
+    # Set SVM type
+    svm.setType(cv2.ml.SVM_ONE_CLASS)
+
+    # Set SVM Kernel to Radial Basis Function (RBF)
+    svm.setKernel(cv2.ml.SVM_POLY)
+
+    # det degree
+    svm.setDegree(2.5)
+
+    # Set parameter C
+    svm.setC(3)
+    svm.setP(0)
+    svm.setCoef0(0)
+    svm.setNu(.5)
+    # Set parameter Gamma
+    svm.setGamma(1)
+
+    # Train SVM on training data
+    dataset = np.squeeze(np.array(dataset))
+    print(dataset.shape)
+    datalabels = np.array(datalabels)
+    print(datalabels)
+    svm.train(dataset, cv2.ml.ROW_SAMPLE, datalabels)
+
+    # Save trained model
+    svm.save("svm_signs_model.yml")
+    print("Training Done")
+    return svm
+
 def train():
     hog = getHOG()
     dataset = []
@@ -79,7 +140,7 @@ def train():
     folderCount = 0
     folderList = getFolderList("Training")
     dataCount = 0
-
+    
     while folderCount < len(folderList):
         imageCount = 0
         imageList, prop = loadImages(folderList[folderCount])
@@ -91,15 +152,17 @@ def train():
             dataset.append(des)
             datalabels.append(int(classId))
             # print(int(classId))
-            cv2.imshow('frame', new_img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+#             cv2.imshow('frame', new_img)
+#             if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 break
             imageCount = imageCount + 1
             dataCount = dataCount + 1
             # print(imageCount)
         folderCount = folderCount + 1
         cv2.destroyAllWindows()
-
+#     print('dataset', len(dataset))
+    # rbf    gamm = 0.9 c =5
+    
     # Set up SVM for OpenCV 3
     svm = cv2.ml.SVM_create()
 
@@ -113,7 +176,7 @@ def train():
     svm.setDegree(2.5)
 
     # Set parameter C
-    svm.setC(2.5)
+    svm.setC(12.5)
 
     # Set parameter Gamma
     svm.setGamma(0.030625)
@@ -123,6 +186,26 @@ def train():
     print(dataset.shape)
     datalabels = np.array(datalabels)
     svm.train(dataset, cv2.ml.ROW_SAMPLE, datalabels)
+    
+    '''
+    #Visualize model
+    x_min, x_max = dataset[:, 0].min() - 1, dataset[:, 0].max() + 1
+    y_min, y_max = datalabels[:, 1].min() - 1, datalabels[:, 1].max() + 1
+    h = (x_max / x_min)/100
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+     np.arange(y_min, y_max, h))
+    
+    plt.subplot(1, 1, 1)
+    Z = svm.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+    
+    plt.scatter(dataset[:, 0], dataset[:, 1], c=datalabels, cmap=plt.cm.Paired)
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.show()
+    '''
 
     # Save trained model
     svm.save("svm_model.yml")
@@ -138,41 +221,63 @@ def test(svm):
     hog = getHOG()
     dataset = []
     datalabels = []
-    folderCount = 0
+    folderCount = 3
     folderList = getFolderList("Testing")
     dataCount = 0
-    while folderCount < len(folderList):
+    signs = [1, 14, 17, 19,21,35, 38, 45]
+    imageCount = 0
+    '''
+    ## Dark image test case --------------------------------------------------------------
+    a = np.zeros((64,64,3), dtype = np.uint8)
+    b = []
+    for i in range(3):
+            
+        new_img = resize(a[0], None)
+        des = hog.compute(new_img)
+        b.append(des)
+    ''' 
+    while folderCount < 4:#len(folderList):
+        print('folder count', folderCount)
         imageCount = 0
         imageList, prop = loadImages(folderList[folderCount])
         if not imageList:
             folderCount = folderCount + 1
             continue
-        # print(prop)
-        print(folderList[folderCount])
         classId = prop[0][-1]
 
         while imageCount < len(imageList):
             new_img = cv2.imread(folderList[folderCount] + prop[imageCount][0])
-            new_img = resize(new_img, None)
+            new_img = resize(new_img, prop[imageCount])
             des = hog.compute(new_img)
             dataset.append(des)
-            datalabels.append(int(classId))
+            datalabels.append(int(0))
             cv2.imshow('frame', new_img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             imageCount = imageCount + 1
             dataCount = dataCount + 1
-            # print(imageCount)
+            
         folderCount = folderCount + 1
         cv2.destroyAllWindows()
+    
+#     dataset = np.squeeze(np.array(b))
+    
     dataset = np.squeeze(np.array(dataset))
+#     op = svm.predict(dataset)
+#     print('op',op)
+    
+#     confidence = 1.0 / (1.0 + np.exp(op[1].ravel()))
+#     print('confidence', confidence)
+# 
+#     datalabels = np.ones(3)
     testResponse = svm.predict(dataset)[1].ravel()
+    print('test response', testResponse)
+    
     count = 0
     for i in range(len(testResponse)):
         if (testResponse[i] - datalabels[i]) != 0.0:
             print('Test Value:', testResponse[i])
             print('Actual Value:', datalabels[i])
-            print(i)
             count = count + 1
 
 
@@ -180,11 +285,14 @@ def test(svm):
     print(datalabels)
     print('Percentage: ', float(len(datalabels)-count)/(len(datalabels))*100)
     print(np.unique(testResponse))
+
     return 0
 
 def main():
     """ Main entry point of the app """
-    svm = train()
+    svm = train_traffic_signs()
+#     svm = train()
+    #     print('SVM',svm)
     test(svm)
     # test()
 
