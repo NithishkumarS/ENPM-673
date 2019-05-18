f821300597464b5a7af41ce6a9e871482e86851c
nantha007 <nantha07@terpmail.umd.edu>
2019-05-18 00:20:06 -0400

    MSER_main.py is updated

--- Traffic_Sign_Detection/MSER_main.py
+++ Traffic_Sign_Detection/MSER_main.py
@@ -29,7 +29,6 @@ def loadImages():
         imageList[i] = "TSR/ProcessedInput/" + str(imageList[i]) + ".jpg"
     return imageList
 
-
 def main():
     """ Main entry point of the app """
     frameCount = 85
@@ -49,35 +48,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    '''
-    r_channel = new_img[:,:,2]
-    g_channel = new_img[:,:,1]
-    b_channel = new_img[:,:,0]
-
-    sum = r_channel + g_channel + b_channel
-    sum[sum==0] = 1
-    r_channel = np.divide(r_channel, sum)
-    g_channel = np.divide(b_channel, sum)
-    b_channel = np.divide(b_channel, sum)
-
-    r = np.uint8(np.maximum(0,np.minimum((r_channel-b_channel),(r_channel-g_channel)) ) )
-    b = np.uint8(np.maximum(0,np.minimum((b_channel-r_channel),(b_channel-g_channel)) ) )
-
-    ret, rr = cv2.threshold(r, 240, 255, cv2.THRESH_BINARY)
-    ret, bb = cv2.threshold(b, 240, 255, cv2.THRESH_BINARY)
-    rb = r | b
-
-    cv2.imshow('rb',r)
-
-    cv2.imshow('frame', new_img)
-    cv2.imshow('r',r)
-    cv2.waitKey(0)
-
-    r = np.copy(new_img)        #  r - 35-45    g- 60-70   b-100 - 115
-    r[:,:,1] = 0
-    r[:,:,2] = 0
-    ret,th1 = cv2.threshold(r[:,:,0],100,115,cv2.THRESH_BINARY)
-    cv2.imshow('red_channel',th1)
-    inp = boundingBox(new_img)
-    '''
